9b3aa2db9dd04909d7d1fe2d09a93c3bc6f02636
ilgarhasanzada <ilqarhsnzad@gmail.com>
2024-09-17 17:53:40 +0400

    update

--- project/config/settings/base.py
+++ project/config/settings/base.py
@@ -148,7 +148,6 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 MEDIA_URL = "media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- project/config/settings/local.py
+++ project/config/settings/local.py
@@ -12,12 +12,16 @@ INSTALLED_APPS += [
     'django_browser_reload'
 ]
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 
 # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
